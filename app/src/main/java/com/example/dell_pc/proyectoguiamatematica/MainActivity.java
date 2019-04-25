a9dddbb9d3086cf16e551e095cdbfbe053f5bf51
DELL-PC <j@hotmail.com>
2019-04-25 16:50:33 -0400

    Pantalla de inicio con medio union de boton

--- app/src/main/java/com/example/dell_pc/proyectoguiamatematica/MainActivity.java
+++ app/src/main/java/com/example/dell_pc/proyectoguiamatematica/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.dell_pc.proyectoguiamatematica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    final Intent intent = new Intent(this, Menu.class);
+    Button iniciar = findViewById(R.id.Iniciar);
+    iniciar.
 }
